541c7f827c27afda73a5779d54a1ef9028f8b73b
Matthew Fischer <mattfisc@gmail.com>
2020-06-05 06:51:24 -0700

    best main practices

--- main.py
+++ main.py
@@ -9,3 +9,25 @@ def process_data(data):
     print("data processing")
     return modified_data
 
+def read_data_from_web():
+    print('read data from the web')
+    data = "data from the web"
+    return data
+
+def write_data_to_database(data):
+    print('writing data to the DATABASE')
+    print(data)
+
+def main():
+    data= read_data_from_web()
+    modified_data = process_data(data)
+    write_data_to_database(data)
+
+if __name__ == "__main__":
+    data = "My data read from the web"
+    print(data)
+    modified_data = process_data(data)
+    print(modified_data)
+
+if __name__  == "__main__":
+    main()
