6a59ade5062a6203d1e057772629a9a16e996507
Matthew Fischer <mattfisc@gmail.com>
2020-06-07 20:13:43 -0700

    fox news scraping titles

--- blogscrape.py
+++ blogscrape.py
@@ -7,16 +7,18 @@ response = requests.get('https://www.foxnews.com/')
 
 soup = BeautifulSoup(response.text,features="html.parser")# features for error in virtual enviroment
 
-name = {soup.find(class_='logo')}
+name={'Fox News'}
 link = {'<a>https://www.foxnews.com/</a>'}
 
-# title ={"Fox News"}
-foxData ={}
 
 #get top 5 articles
 # title = soup.find(class_='info-header').find_next('a').get_text()
 # print(title)
 
-title = soup.select('a')
-print(title)
+lines = soup.find_all(class_='info-header')
 
+for line in lines:
+    #title = line.find()
+    title = line.find(class_='title').get_text()
+    #title = line.find('a').get_text()
+    print(title)
