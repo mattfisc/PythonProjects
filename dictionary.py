b6d46679746810f004d34482683ff1b27672ea4b
Matthew Fischer <mattfisc@gmail.com>
2020-06-08 04:08:45 -0700

    update python basic practice

--- dictionary.py
+++ dictionary.py
@@ -1,7 +1,5 @@
-
-
-import datetime
-
-x = datetime.datetime.now()
-
-print(x.strftime("%I"))
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {} pieces of item number {} for {:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
