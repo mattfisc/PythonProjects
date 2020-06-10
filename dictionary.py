48f280fb65c9404274e085bab32829d086729ae0
Matthew Fischer <mattfisc@gmail.com>
2020-06-10 14:59:46 -0700

    machine learning and data stat

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,27 @@
-import numpy as np
+import numpy
+import matplotlib.pyplot as plt
 
-arr = np.array([1,2,3,4,5,6,7,8,9,10])
+#Create the arrays that represents the values of the x and y axis:
 
-for x in np.nditer(arr[1::2]):
-    print(x)
+x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+
+#NumPy has a method that lets us make a polynomial model:
+
+mymodel = numpy.poly1d(numpy.polyfit(x, y, 3))
+
+#Then specify how the line will display, we start at position 1, and end at position 22:
+
+myline = numpy.linspace(1, 22, 100)
+
+#Draw the original scatter plot:
+
+plt.scatter(x, y)
+
+#Draw the line of polynomial regression:
+
+plt.plot(myline, mymodel(myline))
+
+#Display the diagram:
+
+plt.show()
